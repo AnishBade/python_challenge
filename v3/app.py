658d3a628289b345308cc6a9e35b7fc5d67775f5
anish tekvortex <anish.tekvortex@gmail.com>
2023-12-21 17:26:48 +0545

    v6-full running

--- v3/app.py
+++ v3/app.py
@@ -5,21 +5,22 @@ from fastapi.responses import FileResponse
 
 app = FastAPI()
 downloaded_site_path = 'downloaded_site'
-website_url = 'https://www.bbc.com'
-url = 'www.bbc.com'  # Replace with your desired URL
-
+# website_url = 'https://kantipurtv.com/'
+# url = 'www.kantipurtv.com'  # Replace with your desired URL
+website_url = 'https://ekantipur.com/'
+url = 'www.ekantipur.com' 
 depth_limit = 0  # Set your desired depth level here
 
 # Function to download the website
 def download_website():
     if not os.path.exists(downloaded_site_path):
         os.makedirs(downloaded_site_path)
-        subprocess.run(['wget', '--mirror', '--convert-links','--adjust-extension', 
-                        '--page-requisites', '--no-parent', '--level', str(depth_limit), 
-                        website_url, '-P', downloaded_site_path], capture_output=True, text=True)
         # subprocess.run(['wget', '--mirror', '--convert-links','--adjust-extension', 
-        #                 '--page-requisites', '--no-parent', '-l', '2', 
+        #                 '--page-requisites', '--no-parent', '--level', str(depth_limit), 
         #                 website_url, '-P', downloaded_site_path], capture_output=True, text=True)
+        subprocess.run(['wget', '--mirror', '--convert-links','--adjust-extension', 
+                        '--page-requisites', '--no-parent', '-l', '1', 
+                        website_url, '-P', downloaded_site_path], capture_output=True, text=True)
     print("############  download_finished   ########################")
 
 download_website()
